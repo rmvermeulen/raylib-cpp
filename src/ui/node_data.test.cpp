b77088090daa0bbead3c05f5a7228edd5502d6f9
Rasmus <14540163+rmvermeulen@users.noreply.github.com>
2022-08-27 21:51:13 +0200

    node_data.test: clean up

--- src/ui/node_data.test.cpp
+++ src/ui/node_data.test.cpp
@@ -7,36 +7,34 @@ namespace tests {
 void node_data_test() {
     using namespace testing;
     describe("NodeData", [](const Context& $) {
-        $.it("counts total nodes", [] {
-            {
-                ui::NodeData data{ui::NodeType::Row};
-                expect(data.get_total_count() == 1, "must be 1 total");
-            }
-            {
-                ui::NodeData data{ui::NodeType::Row,
-                                  {ui::NodeData{ui::NodeType::Row}}};
+        $.it("counts a node", [] {
+            ui::NodeData data{ui::NodeType::Row};
+            expect(data.get_total_count() == 1, "must be 1 total");
+        });
+        $.it("counts 2 nodes", [] {
+            ui::NodeData data{ui::NodeType::Row,
+                              {ui::NodeData{ui::NodeType::Row}}};
 
-                expect(data.get_total_count() == 2, "must be 2 total");
-            }
-            {
-                ui::NodeData data{ui::NodeType::Row,
-                                  {ui::NodeData{ui::NodeType::Row},
-                                   ui::NodeData{ui::NodeType::Row}}};
+            expect(data.get_total_count() == 2, "must be 2 total");
+        });
+        $.it("counts 3 nodes", [] {
+            ui::NodeData data{ui::NodeType::Row,
+                              {ui::NodeData{ui::NodeType::Row},
+                               ui::NodeData{ui::NodeType::Row}}};
 
-                expect(data.get_total_count() == 3, "must be 3 total");
-            }
-            {
-                ui::NodeData data{
-                    ui::NodeType::Row,
-                    {ui::NodeData{ui::NodeType::Column,
-                                  {ui::NodeData{ui::NodeType::Row},
-                                   ui::NodeData{ui::NodeType::Row}}},
-                     ui::NodeData{ui::NodeType::Column,
-                                  {ui::NodeData{ui::NodeType::Row},
-                                   ui::NodeData{ui::NodeType::Row}}}}};
+            expect(data.get_total_count() == 3, "must be 3 total");
+        });
+        $.it("counts nested nodes", [] {
+            ui::NodeData data{
+                ui::NodeType::Row,
+                {ui::NodeData{ui::NodeType::Column,
+                              {ui::NodeData{ui::NodeType::Row},
+                               ui::NodeData{ui::NodeType::Row}}},
+                 ui::NodeData{ui::NodeType::Column,
+                              {ui::NodeData{ui::NodeType::Row},
+                               ui::NodeData{ui::NodeType::Row}}}}};
 
-                expect(data.get_total_count() == 7, "must be 7 total");
-            }
+            expect(data.get_total_count() == 7, "must be 7 total");
         });
     });
 }
